532345b14b24714cd31c53ab4e4f3c75dea9f7e8
busyhe <busyhe@qq.com>
2023-09-07 12:30:46 +0800

    update

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # frontend-interview
 
-https://juejin.cn/post/7229157003427119162
+![Alt](https://repobeats.axiom.co/api/embed/3612736b02dbd77fb582d429b550ff2fd417e135.svg "Repobeats analytics image")
 
-https://vue3js.cn/interview/typescript/interface.html
+## 参考资料
 
-https://github.com/yygmind/blog/issues/43
-
-https://github.com/Jacky-Summer/personal-blog
+- https://juejin.cn/post/7229157003427119162
+- https://vue3js.cn/interview/typescript/interface.html
+- https://github.com/yygmind/blog/issues/43
+- https://github.com/Jacky-Summer/personal-blog
--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig, type DefaultTheme } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -21,167 +21,178 @@ export default defineConfig({
     ]
   ],
   themeConfig: {
+    logo: '/logo.jpg',
     search: {
       provider: 'local'
     },
-    nav: [
-      { text: 'Home', link: '/' }
-    ],
-    sidebar: [
-      {
-        text: 'Css',
-        items: [
-          { text: 'opacity、display、visibility', link: '/css/opacityDisplayVisibility' },
-        ]
-      },
-      {
-        text: 'Vue',
-        items: [
-          { text: 'v-show & v-if 的理解', link: '/vue/v-show&v-if' },
-          { text: 'vue 实例挂载的过程', link: '/vue/init' },
-          { text: '生命周期', link: '/vue/lifeCycle' },
-          { text: '首屏加载', link: '/vue/firstScreen' },
-          { text: '双向绑定', link: '/vue/twoWayBinding' },
-          { text: 'watch&computed', link: '/vue/watch&computed' },
-          { text: 'nextTick', link: '/vue/nextTick' },
-          { text: 'observable', link: '/vue/observable' },
-          { text: 'key', link: '/vue/key' },
-          { text: '自定义指令', link: '/vue/customDirectives' },
-          { text: 'vuex', link: '/vue/vuex' },
-        ]
-      },
-      {
-        text: 'Vue3',
-        items: [
-          { text: 'Proxy', link: '/vue3/proxy' },
-        ]
-      },
-      {
-        text: 'React',
-        items: [
-          { text: '生命周期', link: '/react/lifeCycle' },
-          { text: 'state&props', link: '/react/state&props' },
-          { text: 'setState', link: '/react/setState' },
-          { text: '类组件和函数组件', link: '/react/components' },
-          { text: '受控组件&非受控组件', link: '/react/controlledComponents' },
-          { text: 'Hooks', link: '/react/hook' },
-        ]
-      },
-      {
-        text: 'JS',
-        items: [
-          { text: '深拷贝浅拷贝', link: '/js/deepCopy' },
-          { text: '防抖节流', link: '/js/debounce&throttle' },
-          { text: '闭包', link: '/js/closure' },
-          { text: '严格模式', link: '/js/strictMode' },
-          { text: '事件循环', link: '/js/eventLoop' },
-          { text: '事件代理', link: '/js/eventProxy' },
-          { text: '内存泄漏', link: '/js/memoryLeak' },
-          { text: '作用域链', link: '/js/scopeChain' },
-          { text: '原型', link: '/js/prototype' },
-          { text: '继承', link: '/js/inheritance' },
-          { text: 'this', link: '/js/this' },
-          { text: 'typeof&instanceof', link: '/js/typeof&instanceof' },
-          { text: '本地储存', link: '/js/localStorage' },
-          { text: '箭头函数', link: '/js/arrowFunction' },
-          { text: '模块化', link: '/js/modularization' },
-          { text: 'set&map', link: '/js/setMap' },
-          { text: 'call&apply&bind', link: '/js/callApplyBind' },
-          { text: '攻击', link: '/js/attack' },
-        ]
-      },
-      {
-        text: 'ES6',
-        items: [
-          { text: 'var、let、const', link: '/es6/letConst' },
-          { text: 'Promise', link: '/es6/promise' },
-          { text: 'Generator', link: '/es6/generator' },
-          { text: 'Proxy', link: '/es6/proxy' },
-          { text: '装饰器', link: '/es6/decorator' },
-        ]
-      },
-      {
-        text: 'TS',
-        items: [
-          { text: 'TS 的理解', link: '/ts/introduce' },
-          { text: '编译过程', link: '/ts/compilation' },
-          { text: '数据类型', link: '/ts/dataType' },
-          { text: '实用程序类型', link: '/ts/utilityTypes' },
-          { text: '接口', link: '/ts/interface' },
-          { text: '装饰器', link: '/ts/decorators' },
-          { text: '命名空间与模块', link: '/ts/namespace' },
-        ]
-      },
-      {
-        text: 'NodeJS',
-        items: [
-          { text: '框架对比', link: '/node/framework' },
-          { text: 'Stream', link: '/node/stream' },
-          { text: 'Buffer', link: '/node/buffer' },
-          { text: 'EventEmitter', link: '/node/eventEmitter' },
-          { text: '事件循环', link: '/node/eventLoop' },
-          { text: 'JWT', link: '/node/JWT' },
-        ]
-      },
-      {
-        text: 'Http',
-        items: [
-          { text: '地址栏', link: '/http/address' },
-          { text: 'DNS', link: '/http/dns' },
-          { text: 'CDN', link: '/http/cdn' },
-          { text: '2.0', link: '/http/2' },
-          { text: '状态码', link: '/http/status' },
-          { text: '缓存', link: '/http/cache' },
-          { text: 'https', link: '/http/https' },
-        ]
-      },
-      {
-        text: '算法',
-        items: [
-          { text: '数据结构', link: '/algorithm/dataStructure' },
-        ]
-      },
-      {
-        text: 'Webpack',
-        items: [
-          { text: '介绍', link: '/webpack/index' },
-          { text: 'Loader&plugin', link: '/webpack/loader&plugin' },
-          { text: '构建流程', link: '/webpack/build' },
-          { text: '优化前端性能', link: '/webpack/frontend' },
-          { text: '构建速度', link: '/webpack/speed' },
-          { text: '热更新', link: '/webpack/hot' },
-        ]
-      },
-      {
-        text: 'Electron',
-        items: [
-          { text: '介绍', link: '/electron/index' },
-        ]
-      },
-      {
-        text: '设计模式',
-        items: [
-          { text: '介绍', link: '/designPattern/index' },
-        ]
-      },
-      {
-        text: 'build',
-        items: [
-          { text: '性能优化', link: '/build/performance' },
-        ]
-      },
-      {
-        text: '笔试题',
-        items: [
-          { text: '交换a&b', link: '/written/a&b' },
-          { text: "1234=>'4321'", link: '/written/1234' },
-          { text: "两数之和'", link: '/written/sumOfTwoNumbers' },
-        ]
-      }
-    ],
-
+    nav: nav(),
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+      { icon: 'github', link: 'https://github.com/busyhe/frontend-interview' }
+    ],
+    sidebar: sidebar()
   }
 })
+
+function nav(): DefaultTheme.NavItem[] {
+  return [
+    { text: 'Home', link: '/' },
+    { text: 'blog', link: 'https://blog.busyhe.com/' },
+    { text: 'wiki', link: 'https://wiki.busyhe.com/' },
+    { text: 'me', link: 'https://www.busyhe.com/' }
+  ]
+}
+
+function sidebar(): DefaultTheme.SidebarItem[] {
+  return [
+    {
+      text: 'Css',
+      items: [
+        { text: 'opacity、display、visibility', link: '/css/opacityDisplayVisibility' },
+      ]
+    },
+    {
+      text: 'Vue',
+      items: [
+        { text: 'v-show & v-if 的理解', link: '/vue/v-show&v-if' },
+        { text: 'vue 实例挂载的过程', link: '/vue/init' },
+        { text: '生命周期', link: '/vue/lifeCycle' },
+        { text: '首屏加载', link: '/vue/firstScreen' },
+        { text: '双向绑定', link: '/vue/twoWayBinding' },
+        { text: 'watch&computed', link: '/vue/watch&computed' },
+        { text: 'nextTick', link: '/vue/nextTick' },
+        { text: 'observable', link: '/vue/observable' },
+        { text: 'key', link: '/vue/key' },
+        { text: '自定义指令', link: '/vue/customDirectives' },
+        { text: 'vuex', link: '/vue/vuex' },
+      ]
+    },
+    {
+      text: 'Vue3',
+      items: [
+        { text: 'Proxy', link: '/vue3/proxy' },
+      ]
+    },
+    {
+      text: 'React',
+      items: [
+        { text: '生命周期', link: '/react/lifeCycle' },
+        { text: 'state&props', link: '/react/state&props' },
+        { text: 'setState', link: '/react/setState' },
+        { text: '类组件和函数组件', link: '/react/components' },
+        { text: '受控组件&非受控组件', link: '/react/controlledComponents' },
+        { text: 'Hooks', link: '/react/hook' },
+      ]
+    },
+    {
+      text: 'JS',
+      items: [
+        { text: '深拷贝浅拷贝', link: '/js/deepCopy' },
+        { text: '防抖节流', link: '/js/debounce&throttle' },
+        { text: '闭包', link: '/js/closure' },
+        { text: '严格模式', link: '/js/strictMode' },
+        { text: '事件循环', link: '/js/eventLoop' },
+        { text: '事件代理', link: '/js/eventProxy' },
+        { text: '内存泄漏', link: '/js/memoryLeak' },
+        { text: '作用域链', link: '/js/scopeChain' },
+        { text: '原型', link: '/js/prototype' },
+        { text: '继承', link: '/js/inheritance' },
+        { text: 'this', link: '/js/this' },
+        { text: 'typeof&instanceof', link: '/js/typeof&instanceof' },
+        { text: '本地储存', link: '/js/localStorage' },
+        { text: '箭头函数', link: '/js/arrowFunction' },
+        { text: '模块化', link: '/js/modularization' },
+        { text: 'set&map', link: '/js/setMap' },
+        { text: 'call&apply&bind', link: '/js/callApplyBind' },
+        { text: '攻击', link: '/js/attack' },
+      ]
+    },
+    {
+      text: 'ES6',
+      items: [
+        { text: 'var、let、const', link: '/es6/letConst' },
+        { text: 'Promise', link: '/es6/promise' },
+        { text: 'Generator', link: '/es6/generator' },
+        { text: 'Proxy', link: '/es6/proxy' },
+        { text: '装饰器', link: '/es6/decorator' },
+      ]
+    },
+    {
+      text: 'TS',
+      items: [
+        { text: 'TS 的理解', link: '/ts/introduce' },
+        { text: '编译过程', link: '/ts/compilation' },
+        { text: '数据类型', link: '/ts/dataType' },
+        { text: '实用程序类型', link: '/ts/utilityTypes' },
+        { text: '接口', link: '/ts/interface' },
+        { text: '装饰器', link: '/ts/decorators' },
+        { text: '命名空间与模块', link: '/ts/namespace' },
+      ]
+    },
+    {
+      text: 'NodeJS',
+      items: [
+        { text: '框架对比', link: '/node/framework' },
+        { text: 'Stream', link: '/node/stream' },
+        { text: 'Buffer', link: '/node/buffer' },
+        { text: 'EventEmitter', link: '/node/eventEmitter' },
+        { text: '事件循环', link: '/node/eventLoop' },
+        { text: 'JWT', link: '/node/JWT' },
+      ]
+    },
+    {
+      text: 'Http',
+      items: [
+        { text: '地址栏', link: '/http/address' },
+        { text: 'DNS', link: '/http/dns' },
+        { text: 'CDN', link: '/http/cdn' },
+        { text: '2.0', link: '/http/2' },
+        { text: '状态码', link: '/http/status' },
+        { text: '缓存', link: '/http/cache' },
+        { text: 'https', link: '/http/https' },
+      ]
+    },
+    {
+      text: '算法',
+      items: [
+        { text: '数据结构', link: '/algorithm/dataStructure' },
+      ]
+    },
+    {
+      text: 'Webpack',
+      items: [
+        { text: '介绍', link: '/webpack/index' },
+        { text: 'Loader&plugin', link: '/webpack/loader&plugin' },
+        { text: '构建流程', link: '/webpack/build' },
+        { text: '优化前端性能', link: '/webpack/frontend' },
+        { text: '构建速度', link: '/webpack/speed' },
+        { text: '热更新', link: '/webpack/hot' },
+      ]
+    },
+    {
+      text: 'Electron',
+      items: [
+        { text: '介绍', link: '/electron/index' },
+      ]
+    },
+    {
+      text: '设计模式',
+      items: [
+        { text: '介绍', link: '/designPattern/index' },
+      ]
+    },
+    {
+      text: 'build',
+      items: [
+        { text: '性能优化', link: '/build/performance' },
+      ]
+    },
+    {
+      text: '笔试题',
+      items: [
+        { text: '交换a&b', link: '/written/a&b' },
+        { text: "1234=>'4321'", link: '/written/1234' },
+        { text: "两数之和'", link: '/written/sumOfTwoNumbers' },
+      ]
+    }
+  ]
+}
